533d1c5966d191d61fc244a579767fb30d7e0089
renan <renan@pop-os.localdomain>
2024-02-27 22:28:08 -0300

    add templates to build

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,48 @@ const express = require("express");
 const crypto = require("crypto");
 const router = express.Router();
 const generatePdf = require("../controller/generatePdf");
+const handlebars = require('handlebars')
+const fs = require('fs');
+const path = require("path");
+
+handlebars.registerHelper('toFixed', function (value, places = 2) {
+  if (!value) return value
+  return `${Number(value).toFixed(places)}`
+})
+
+handlebars.registerHelper('toRound', function (value) {
+  if (!value) return value
+  return `${Math.round(value)}`
+})
+
+handlebars.registerHelper('reduce', function (value, factor) {
+  if (!value) return value
+  return `${value / factor}`
+})
+
+handlebars.registerHelper('cable', function (value) {
+  return value === 'Fiber Core' ? 'Alma de Fibra' : 'Alma Cabo Independ.'
+})
+
+handlebars.registerHelper('percent', function (value) {
+  return (value * 100).toFixed(2)
+})
 
 router.get("/", (_, res) => {
   res.send({health: true})
 });
+
+router.post('/memorial', async (req, res) => {
+  const {data, title} = req.body
+  let html = await fs.readFileSync(path.join(__dirname, '../templates/memorial.hbs'))
+  html = handlebars.compile(html.toString())(data)
+  let result = await generatePdf(html);
+  console.log('title', title)
+  let filename = `${title}-${+new Date()}`
+  res.attachment(`${filename}.pdf`);
+  res.contentType("application/pdf");
+  res.send(result);
+})
 
 router.post("/generate-pdf", async (req, res) => {
   const {content, title} = req.body
